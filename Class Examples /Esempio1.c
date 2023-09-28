d6e16e757a9a2fba943641314297161c7cf7e9ad
Lorenzo Gardini <lo.gardini@reply.it>
2023-09-28 14:34:49 +0200

    add example1

---
+++ Class Examples /Esempio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 75;
+    char lettera = 'k';
+
+    printf("%d\n", numero);
+    printf("%c\n", lettera);
+
+    printf("%c\n", numero);
+    printf("%d\n", lettera);
+
+    return 0;
+}
