30aaef42ec3f2d430e8f50fd5f0fd5931463d27e
Alessandro Gardini <lo.gardini@reply.it>
2023-09-29 19:27:08 +0200

    adding code for ex1 and adding the heading

--- Home Execises/Esercizio1.c
+++ Home Execises/Esercizio1.c
@@ -1,33 +1,19 @@
 /*
- * @file Esercizio1.c
- * @author Filo, Gardo, Rossi
- * @brief 
- * @version 0.1
- * @date 2023-09-25
- * 
- * @copyright Copyright (c) 2023
- *
- */
 
-/*
-* %TODO
-* Scrivere un programma C che dichiari una variabile per ogni tipo studiato (usando anche i qualificatori short, long, signed e unsigned), 
-* ne stampi il valore e l’occupazione di memoria in byte. Utilizzare TUTTI gli argomenti della funzione printf() studiati e TUTTE le diverse varianti 
-* (modificatore, campoMinimo, precisione, modificatoreLunghezza). 
-* Per ogni chiamata alla funzione printf(), mostrare a video anche il numero di caratteri che quest’ultima ha stampato. 
-* Stampare inoltre il valore minimo e il valore massimo ammissibile per ogni tipo utilizzato. Commentare adeguatamente il programma.
-*
+Alessandro Gardini n. matricola: 0001114867
+Filippo Greppi n. matricola:
+Lorenzo Rossi n. matricola:
+
+Exercise number: 1
+
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-
-#include <stdio.h>
-
-int main() {
+int main(){
+    //declaration-->
     char c;
-    int i=27, l=5;
+    int i = 27, l = 5;
     short int si;
     long int li;
     unsigned char uc;
@@ -37,23 +23,38 @@ int main() {
     double d;
     long double ld;
 
-    printf("\n valore:%d dimensione:%d", c, sizeof(c) );
-    printf("\nvalore:%d dimensione:%d", i, sizeof(i) );
-    printf("\nvalore:%d dimensione:%d", si, sizeof(si) );
-    printf("\nvalore:%d dimensione:%d", li, sizeof(li) );
-    printf("\nvalore:%d dimensione:%d", uc, sizeof(uc) );
-    printf("\nvalore:%d dimensione:%d", ui, sizeof(ui) );
-    printf("\nvalore:%d dimensione:%d", ul, sizeof(ul) );
-    printf("\nvalore:%d dimensione:%d", f, sizeof(f) );
-    printf("\nvalore:%d dimensione:%d", d, sizeof(d) );
-    printf("\nvalore:%d dimensione:%d", ld, sizeof(ld) );
-    l = printf("\n %-d,numero di caratteri %d", i, l ); //da finire
-    l = printf("\n %+d,numero di caratteri %d", i, l);
-    l = printf("\n % d", i, l );
-    printf("\n %#d");
-    printf("\n %0d");
-    printf("\n %10d");
-    printf("\n %.10d");
-    printf("\n %ld");
+    //stampo variabili con le dimensioni che occupano
+    printf("Stampo variabili con le dimensioni che occupano-->");
+    printf("\nvalore:%d -- dimensione:%d", c, sizeof(c));
+    printf("\nvalore:%d -- dimensione:%d", i, sizeof(i));
+    printf("\nvalore:%d -- dimensione:%d", si, sizeof(si));
+    printf("\nvalore:%d -- dimensione:%d", li, sizeof(li));
+    printf("\nvalore:%d -- dimensione:%d", uc, sizeof(uc));
+    printf("\nvalore:%d -- dimensione:%d", ui, sizeof(ui));
+    printf("\nvalore:%d -- dimensione:%d", ul, sizeof(ul));
+    printf("\nvalore:%d -- dimensione:%d", f, sizeof(f));
+    printf("\nvalore:%d -- dimensione:%d", d, sizeof(d));
+    printf("\nvalore:%d -- dimensione:%d", ld, sizeof(ld));
+    printf("\n\n");
+
+    //stampo usando i vari modificatori e il numero dei caratteri che usano
+    printf("Stampo usando i vari modificatori e il numero dei caratteri che usano-->");
+    l = printf("\n %-d", i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n %+d", i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n % d", i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n %#d",i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n %0d", i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n %10d", i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n %.10d", i);
+    printf(" -- numero di caratteri: %d", l);
+    l = printf("\n %ld", i);
+    printf(" -- numero di caratteri: %d", l);
+
     return 0;
 }
