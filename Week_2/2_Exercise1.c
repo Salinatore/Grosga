af331fde08410eca660795483f1570c6de2aab8c
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-02 14:57:50 +0200

    Aggiornamento

--- Week_2/2_Exercise1.c
+++ Week_2/2_Exercise1.c
@@ -1,3 +1,22 @@
-//
-// Created by Gardini on 02/10/23.
-//
+/*
+* ESERCIZIO 1
+* Scrivere un programma C che mostri l’utilizzo di tutti gli operatori studiati.
+* Il programma deve inoltre mostrare il comportamento scorretto che si ottiene utilizzando tipi non appropriati alla dimensione delle variabili, quello che si ottiene usando più operatori con side
+* effect all’interno della stessa istruzione, quello che si ottiene quando le regole di conversione implicita sono applicate e quando si supera il range dei valori disponibili per un tipo di dato.
+*
+* Il programma deve poi mostrare l’utilizzo di tutte le funzioni matematiche della libreria math.h.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+
+
+
+
+
+    printf("\n\n");
+    return 0;
+}
