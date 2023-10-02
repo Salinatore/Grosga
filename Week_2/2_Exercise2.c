08b338b2b97b7211932a18c69c3b39548e5612e8
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-02 15:05:17 +0200

    Removed scanfolinf

--- Week_2/2_Exercise2.c
+++ Week_2/2_Exercise2.c
@@ -1,3 +1,24 @@
-//
-// Created by Gardini on 02/10/23.
-//
+/*
+ * ESERCIZIO 2
+ * L’utente pensa ad un numero e il computer lo deve indovinare compiendo le seguenti operazioni.
+ * L’utente fissa un intervallo entro cui generare il valore da indovinare (min, max). Se i valori inseriti non rispettano il vincolo 0<min≤max, l’operazione di lettura va ripetuta
+ * L’utente pensa ad un numero compreso tra min e max.
+ * Il programma cerca di indovinare la scelta dell’utente e propone un numero. A seconda della situazione l’utente deve dire al programma se
+ * o il numero da indovinare è più piccolo;
+ * il numero da indovinare è più grande; ha indovinato.
+ * Scrivere una versione di programma in cui non c'è limite al numero di tentativi che l’utente può fare per indovinare il numero e scriverne un'altra in cui invece viene posto tale limite.
+ * Il programma deve stampare il numero di tentativi fatti.
+ * /
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+
+
+
+
+    printf("\n\n");
+    return 0;
+}
