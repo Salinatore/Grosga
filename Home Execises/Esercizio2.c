ae76fd55873e43e2673cb0424fe4dbde5d0b027e
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-02 13:11:18 +0200

    Update Esercizio2.c

--- Home Execises/Esercizio2.c
+++ Home Execises/Esercizio2.c
@@ -1,37 +1,29 @@
-/*
-
-Alessandro Gardini n. matricola: 0001114867
-Filippo Greppi n. matricola:
-Lorenzo Rossi n. matricola:
-
-Exercise number: 2
-
+/**
+ * @file Esercizio2.c
+ * @author Alessandro Gardini n. matricola: 0001114867, Filippo Greppi n. matricola: 0001114837, Lorenzo Rossi n. matricola: 0001114876
+ * @version 0.1
+ * @date 2023-10-01
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-float converter(float mi_gal);
 
 int main() {
     float mi, gal;
 
+    // ask for input
     printf("\nInserisci il numero di miglia percorse: ");
     scanf("%f", &mi);
     printf("\nInserisci il numero di galloni di benzina comsumati: ");
     scanf("%f", &gal);
 
-    float mpg = mi/gal;
+    // miles per gallon
+    float mpg = mi / gal;
     printf("\nLe miglia per galloni sono: %.2f", mpg);
 
-    float lp100km = converter(mpg);
+    // conversion (miles per gallon to L/100Km)
+    float lp100km = 235.215 / mpg;
     printf("\nLitri per 100km: %.3f", lp100km);
 
     printf("\n\n");
-    system("pause");
     return 0;
-}
-
-float converter(float mi_gal) {
-    return mi_gal * 235.2143;
 }
