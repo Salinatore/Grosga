1555e767af498f36d37eefd593e66745abcab618
Lorenzo Gardini <lo.gardini@reply.it>
2023-10-03 12:49:44 +0200

    adding control max_try and starting the main dowhile

--- Week_2/2_Exercise2.c
+++ Week_2/2_Exercise2.c
@@ -11,9 +11,11 @@
  */
 
 #include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    int n, min=0, max=0;
+
+    int n, min=0, max=0, max_try=-1;
 
     //controlla che l'intervallo inserito rispetti il vincolo 0<min<=max
     while(min<0 || min>=max) {
@@ -21,7 +23,7 @@ int main() {
         scanf("%d,%d", &min, &max);
 
         if(min<0 || min>=max)
-            printf("Il minimo non può essere più grande del massimo, rispetta il vincolo 0<min<=max!\n");
+            printf("  Il minimo non può essere più grande del massimo, rispetta il vincolo 0<min<=max!!\n");
     }
     //controlla che n sia all'interno dell'intervallo, altrimenti richiedi il valore
     while(n>=min && n>=max) {
@@ -29,9 +31,23 @@ int main() {
         scanf("%d", &n);
 
         if(n>=min && n>=max)
-            printf("Rispetta l'intervallo: %d<=numero<=%d\n", min, max);
+            printf("  Rispetta l'intervallo: %d<=numero<=%d!!\n", min, max);
     }
 
+    //controlla se i tentativi siano maggiori di 0
+    while(max_try<0){
+        printf("\nInserisci i tentativi massimi: ");
+        scanf("%d", &max_try);
+
+        if(max_try<0){
+            printf("  Inserisci un numero positivo!!\n");
+        }
+    }
+
+    do{
+        max_try--;
+        printf("il tuo numero è %d", )
+    } while(max_try>=0);
 
     printf("\n\n");
     return 0;
