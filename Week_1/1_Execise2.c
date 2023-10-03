78076b99f742d0068bde26e2bfb632c0bd819bc8
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-03 10:27:56 +0200

    Added 2 const, updated lp100km var

--- Week_1/1_Execise2.c
+++ Week_1/1_Execise2.c
@@ -1,14 +1,16 @@
 /**
  * @file Esercizio2.c
  * @author Alessandro Gardini n. matricola: 0001114867, Filippo Greppi n. matricola: 0001114837, Lorenzo Rossi n. matricola: 0001114876
- * @version 0.1
- * @date 2023-10-01
+ * @version 0.2
+ * @date 2023-10-03
 */
 
 #include <stdio.h>
 
 int main() {
     float mi, gal;
+    const float mi_to_km = 1.609;
+    const float gal_to_lit = 3.785;
 
     // ask for input
     printf("\nInserisci il numero di miglia percorse: ");
@@ -21,7 +23,7 @@ int main() {
     printf("\nLe miglia per galloni sono: %.2f", mpg);
 
     // conversion (miles per gallon to L/100Km)
-    float lp100km = 235.215 / mpg;
+    float lp100km = (gal * gal_to_lit) / (100 * mi * mi_to_km);
     printf("\nLitri per 100km: %.3f", lp100km);
 
     printf("\n\n");
