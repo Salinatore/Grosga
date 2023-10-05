7296311d14dea4315411084304dc2603f868c8b4
rossi <lorenzo.rossi50@studio.unibo.it>
2023-10-05 15:45:50 +0200

    es 1 week2

--- Week_2/2_Exercise1.c
+++ Week_2/2_Exercise1.c
@@ -1,8 +1,13 @@
 /*
 * ESERCIZIO 1
-* Scrivere un programma C che mostri l’utilizzo di tutti gli operatori studiati.
-* Il programma deve inoltre mostrare il comportamento scorretto che si ottiene utilizzando tipi non appropriati alla dimensione delle variabili, quello che si ottiene usando più operatori con side
-* effect all’interno della stessa istruzione, quello che si ottiene quando le regole di conversione implicita sono applicate e quando si supera il range dei valori disponibili per un tipo di dato.
+* Scrivere un programma C che mostri l’utilizzo di tutti gli operatori studiati.#
+* Il programma deve inoltre mostrare il comportamento scorretto che si ottiene utilizzando tipi non appropriati
+* alla dimensione delle variabili,#
+ *
+* quello che si ottiene usando più operatori con side effect all’interno della stessa istruzione,#
+ *
+*  quello che si ottiene quando le regole di conversione implicita sono applicate #
+* quando si supera il range dei valori disponibili per un tipo di dato.#
 *
 * Il programma deve poi mostrare l’utilizzo di tutte le funzioni matematiche della libreria math.h.
 *
@@ -12,7 +17,7 @@
 #include <math.h>
 
 int main() {
-    int n;
+    int n, b =0, c= 1;
     float f, decimale;
 
     n = 3;
@@ -36,13 +41,47 @@ int main() {
     n /= 3;
     printf("\n%d", n);
 
+    n = (b++, c++);
+    printf("\n%d", n);
+
     //casting
     decimale = f - (int)f;
     printf("\n%f", f);
 
+    //utilizzo tipi non appropriati alla dimensione delle variabil
+    printf("\n%f", n);
+    //più operatori con side effect
+    n = 1;
+    b = n*n++;
+    printf("\n%d", b);
+    //conversione implicita sono applicate
+    char h;
+    int i;
+    i = h;
+    // si supera il range dei valori disponibili
+    short int s;
+    int x = 999999;
+    s = x;
+    printf("\n%d", s);
 
-    // stampa delle funzioni matemtiche di math.h
-    printf("\nRadice");
+    double r = 0.0, y = 2.25, o = 3.5;
+    r = floor(y);
+    printf("\n%f", r);
+    r = ceil(y);
+    printf("\n%f", r);
+    r = fabs(y);
+    printf("\n%f", r);
+    r = log(y);
+    printf("\n%f", r);
+    r = log10(y);
+    printf("\n%f", r);
+    r = exp(y);
+    printf("\n%f", r);
+    r = sqrt(y);
+    printf("\n%f", r);
+    r = pow(o, y);
+    printf("\n%f", r);
+
 
 
     printf("\n\n");
