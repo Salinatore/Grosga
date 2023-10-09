f3002ffcdb2c821dff59f9d15ba02c4c3344f3fe
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-09 13:19:34 +0200

    started

---
+++ Week_3/3_Exercise1.c
@@ -0,0 +1,26 @@
+/**
+ * @file 3_Exercise1.c
+ * @author Alessandro Gardini n. matricola: 0001114867, Filippo Greppi n. matricola: 0001114837, Lorenzo Rossi n. matricola: 0001114876
+ * @version 0.1 
+ * @date 09/10/23
+*/
+
+/*
+ * ESERCIZIO 1
+ * Indovina il continente
+ * In questo gioco, il computer visualizza una coppia di coordinate generata in maniera random sullo schermo per un breve periodo di tempo.
+ * Il giocatore deve indovinare a quale continente appartiene la coppia di coordinate generate; fino a quando l’utente indovina, il gioco prosegue con
+ * la generazione di una nuova coppia di coordinate. L'obiettivo è quello di continuare il processo il più a lungo possibile.
+ * Al termine del gioco il programma calcolerà un punteggio ottenuto, quantomeno sulla base del numero di iterazioni eseguite
+ * (è possibile valutare altri criteri, ad esempio, la velocità con cui l’utente risponde, oppure un calcolo basato sulla difficoltà delle coordinate prodotte)
+ * e inviterà il giocatore a giocare di nuovo.
+ * N.B.: i margini che definiscono i confini dei vari continenti sono decisi da voi
+ */
+#include <stdio.h>
+
+int main() {
+    // Your code here
+
+    printf("\n\n");
+    return 0;
+}
