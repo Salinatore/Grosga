0357c002c0e902a729766fdc26b0cb806532ef28
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-13 09:45:51 +0200

    Testo

---
+++ Week_3/3_Exercise2.c
@@ -0,0 +1,22 @@
+/**
+ * @file 3_Exercise2.c
+ * @author Alessandro Gardini n. matricola: 0001114867, Filippo Greppi n. matricola: 0001114837, Lorenzo Rossi n. matricola: 0001114876
+ * @version 0.1 
+ * @date 13/10/23
+*/
+
+/*
+ * Scrivere un programma che legga da tastiera un testo terminato dal carattere # e poi stampi le statistiche relative al numero di spazi,
+ * al numero di caratteri di nuova linea, al numero di caratteri letti, al numero di quelli maiuscoli, etc.,
+ * utilizzando almeno una volta tutte le funzioni dell’header ctype.h.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    // Your code here
+
+    printf("\n\n");
+    return 0;
+}
