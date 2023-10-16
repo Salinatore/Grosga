84db93dcb1f6c2baa36eaa2656e2034b3a4b3cb5
rossi <rossilorenzo04@gmail.com>
2023-10-16 14:59:14 +0200

    ess 1 week 4

--- Week_4/4_Exercise2.c
+++ Week_4/4_Exercise2.c
@@ -16,8 +16,71 @@
  */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
+    srand(time(NULL));
+    char array[10][10], lettere = 'A';
+    int x = 0, y = 0, d, f= 0;
+    for (int i = 0; i < 10; i++){
+        for(int u = 0; u < 10; u++){
+            array[i][u] = '.';
+        }
+    }
+    for (int i = 0; i < 10; i++){
+        for(int u = 0; u < 10; u++){
+           printf(" %c", array[i][u]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    array[0][0] = lettere;
+    lettere ++;
+    do{
+        d = rand() % 4;
+        if( d == 0 && x+1 <= 10 && array[x+1][y] == '.'){
+            x++;
+            array[x][y] = lettere;
+            lettere++;
+
+        }
+        if( d == 1 && y-1 >= 0 && array[x][y-1] == '.'){
+            y--;
+            array[x][y] = lettere;
+            lettere++;
+
+        }
+        if( d == 2 && x-1 >= 0 && array[x-1][y] == '.'){
+            x--;
+            array[x][y] = lettere;
+            lettere++;
+
+        }
+        if( d == 3 && y+1 <= 10 && array[x][y+1] == '.'){
+            y++;
+            array[x][y] = lettere;
+            lettere++;
+
+        }
+        if(lettere == 'Z') lettere = 'A';
+        if(array[x+1][y] != '.' || x+1 > 10){
+            if(array[x-1][y] != '.' || x-1 < 0){
+               if(array[x][y+1] != '.' || y+1 > 10){
+                   if(array[x][y-1] != '.' || y-1 < 0){
+                       break;
+                   }
+               }
+            }
+        }
+
+    } while (f = 1);
+    for (int i = 0; i < 10; i++){
+        for(int u = 0; u < 10; u++){
+            printf(" %c", array[i][u]);
+        }
+        printf("\n");
+    }
     // Your code here
 
     printf("\n\n");
