13c017acb08d14d73f357927b2f1fd75750c2333
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-10-16 13:13:42 +0200

    New file

---
+++ Week_4/4_Exercise1.c
@@ -0,0 +1,26 @@
+/**
+ * @file 4_Exercise1.c
+ * @author Alessandro Gardini n. matricola: 0001114867, Filippo Greppi n. matricola: 0001114837, Lorenzo Rossi n. matricola: 0001114876
+ * @version 0.1 
+ * @date 16/10/23
+*/
+
+/**
+ * @ESERCIZIO1
+ * Siano dati due vettori di interi di lunghezza differente.
+ * Scrivere un programma C che inserisca i valori da tastiera e che lo faccia anche generandoli in maniera pseudo-casuale.
+ * Il programma deve poi generare un terzo vettore contenente l’intersezione tra i due vettori di input.
+ * Implementare sia la variante in cui le ripetizioni di un elemento nei vettori di input vengono riportate anche nel vettore risultato,
+ * sia quella in cui le ripetizioni non sono inserite nel risultato. Implementare poi la generazione di un quarto vettore contenente
+ * l'unione dei due vettori di input. Il programma deve inoltre stampare gli indirizzi di memoria occupati dagli elementi dei vettori e
+ * la loro dimensione totale in termini di numero di byte.
+ */
+
+#include <stdio.h>
+
+int main() {
+    // Your code here
+
+    printf("\n\n");
+    return 0;
+}
---
+++ Week_4/4_Exercise2.c
@@ -0,0 +1,25 @@
+/**
+ * @file 4_Exercise2.c
+ * @author Alessandro Gardini n. matricola: 0001114867, Filippo Greppi n. matricola: 0001114837, Lorenzo Rossi n. matricola: 0001114876
+ * @version 0.1 
+ * @date 16/10/23
+*/
+
+/**
+ * @ESERCIZIO2
+ * Scrivete un programma che generi una “passeggiata aleatoria” (random walk) in un array bidimensionale di dimensione 10×10.
+ * L’array sarà riempito di caratteri (inizialmente da punti). Il programma dovrà muoversi di elemento in elemento spostandosi ogni volta
+ * di un passo in direzione su, giù, destra o sinistra. Gli elementi visitati andranno etichettati con le lettere dalla A alla Z,
+ * nell’ordine in cui vengono visitati. E’ importante controllare ad ogni passo che la passeggiata non esca dall’array e che non ritorni
+ * su posizioni già visitate. Quando si verifica una di queste condizioni, provare le altre direzioni.
+ * Se tutte e quattro le direzioni sono bloccate, il programma deve terminare.
+ */
+
+#include <stdio.h>
+
+int main() {
+    // Your code here
+
+    printf("\n\n");
+    return 0;
+}
