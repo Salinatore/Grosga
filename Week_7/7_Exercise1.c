1d02700abcf8a300cdaabfceefaa45b844631461
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-11-06 19:07:32 +0100

    non va un cazzo ma ok

--- Week_7/7_Exercise1.c
+++ Week_7/7_Exercise1.c
@@ -16,9 +16,9 @@
 struct credit {
     char loanID[MAX];
     char customerID[MAX];
-    bool loanStatus;
+    char loanStatus[MAX];
     int currentLoanAmount;
-    bool term;
+    char term[MAX];
     int creditScore;
     int annualIncome;
     char yearJob[MAX];
@@ -27,35 +27,64 @@ struct credit {
     float monthlyDebt;
     float yearsOfCreditHistory;
     float monthsSinceLastDelinquent;
-    int NumberOfOpenAccounts;
+    int numberOfOpenAccounts;
     int numberOfCreditProblems;
     int currentCreditBalance;
     int maximumOpenCredit;
     int bankruptcies;
     int taxLiens;
-} cr;
+};
 
-void fillStructFromFile(const char *filename, struct credit *s);
+int linesofFile(FILE *myfile);
+void fillData(FILE *myfile, struct credit ar[], int ar_length);
+void printDataset(struct credit ar[], int ar_length);
 
 int main() {
-    const char *myfile = "Week_7/credit.csv";
-    fillStructFromFile(myfile, &cr);
-
-
-    printf("\n\n");
-    return 0;
-}
-
-void fillStructFromFile(const char *filename, struct credit *s) { //da fixare
-    FILE *fp =fopen(filename, "r+");
+    FILE *fp = fopen("Week_7/credit.csv", "r"); //TODO da cambiare quando si consengna
 
     if(fp == NULL) {
         printf("\nErrore nell'apertura del file");
-        exit(1);
+        return EXIT_FAILURE;
+    } else {
+        int n_row = linesofFile(fp);
+
+        struct credit *data = malloc(n_row * sizeof(struct credit));
+
+        struct credit data[n_row];
+
+        fillData(fp,data, n_row); //write into the array all the data
+        printDataset(data, n_row);
+
+        fclose(fp);
+        printf("\n\n");
+        return EXIT_SUCCESS;
     }
-    //TODO
-    /*
-     * add a way to read the file into structure
-     */
-    fclose(fp);
+}
+
+int linesofFile(FILE *myfile) {
+    int n = 0;
+    for(char c = getc(myfile); c != EOF; c = getc(myfile)) //count the number of lines
+        if (c == '\n')
+            n++;
+
+    return n;
+}
+
+void fillData(FILE *myfile, struct credit ar[], int ar_length) {
+    for(int i = 0; i < ar_length; i++)
+        fscanf(myfile, "%s,%s,%s,%d,%s,%d,%d,%s,%s,%s,%f,%f,%f,%d,%d,%d,%d,%d,%d", \
+                ar[i-1].loanID, ar[i-1].customerID, ar[i-1].loanStatus, ar[i-1].currentLoanAmount, ar[i-1].term, ar[i-1].creditScore, ar[i-1].annualIncome,     \
+                ar[i-1].yearJob, ar[i-1].homeOwnership, ar[i-1].purpose, ar[i-1].monthlyDebt, ar[i-1].yearsOfCreditHistory, ar[i-1].monthsSinceLastDelinquent,  \
+                ar[i-1].numberOfOpenAccounts, ar[i-1].numberOfCreditProblems, ar[i-1].currentCreditBalance, ar[i-1].maximumOpenCredit, ar[i-1].bankruptcies,    \
+                ar[i-1].taxLiens);
+}
+
+void printDataset(struct credit ar[], int ar_length) {
+    for(int i = 1; i <= ar_length; i++) {
+        printf("\n%s,%s,%s,%d,%s,%d,%d,%s,%s,%s,%f,%f,%f,%d,%d,%d,%d,%d,%d", \
+                ar[i-1].loanID, ar[i-1].customerID, ar[i-1].loanStatus, ar[i-1].currentLoanAmount, ar[i-1].term, ar[i-1].creditScore, ar[i-1].annualIncome,     \
+                ar[i-1].yearJob, ar[i-1].homeOwnership, ar[i-1].purpose, ar[i-1].monthlyDebt, ar[i-1].yearsOfCreditHistory, ar[i-1].monthsSinceLastDelinquent,  \
+                ar[i-1].numberOfOpenAccounts, ar[i-1].numberOfCreditProblems, ar[i-1].currentCreditBalance, ar[i-1].maximumOpenCredit, ar[i-1].bankruptcies,    \
+                ar[i-1].taxLiens);
+    }
 }
