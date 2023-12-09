d51ed57483f360a445997ea13faeb1c48dab8b57
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2023-12-09 10:19:10 +0100

    manca l'algoritmo find-s

--- Week_8/8_Exercise1.c
+++ Week_8/8_Exercise1.c
@@ -6,10 +6,15 @@
 */
 
 #include <stdio.h>
-#include "stdbool.h"
-#define MAX 256
+#include <stdbool.h>
+#include "string.h"
 
-struct ristorante {
+#define MAX 256
+#define WARNING "\x1b[31m"
+#define RESET "\x1b[0m"
+#define PATH "\x1b[36m"
+
+typedef struct {
     bool alternativa; //c’è un ristorante nei paraggi (vero, falso)
     bool bar; //il ristorante ha un’area bar per l’attesa (vero, falso)
     bool giorno; //giorno della settimana in cui si vuole andare al ristorante (vero se venerdì oppure sabato, falso diversamente)
@@ -20,27 +25,69 @@ struct ristorante {
     bool prenotazione; //abbiamo prenotato (vero, falso)
     char tipo[MAX]; //tipo di ristorante (italiano, francese, fast-food, thai)
     unsigned int attesa_stimata; //stima del tempo di attesa (<10, 10-29, 30-60, >60)
-};
+} Ristorante;
 
-bool aspettiamo(struct ristorante);
+bool aspettiamo(Ristorante);
 
 int main() {
-    int scelta;
+    char retry;
     do {
-        printf("\n1: File\n2: Terminale\nCome vuoi inserire i dati:");
-        scanf("%1d", &scelta);
-        if(scelta != 1 && scelta != 2)
-            printf("Inserisci un'opzione valida!\n");
-    } while(scelta != 1 && scelta != 2);
+        int scelta;
+        do {
+            printf("\n1: File\n2: Terminale\nCome vuoi inserire i dati: ");
+            scanf("%1d", &scelta);
+            if(scelta != 1 && scelta != 2)
+                printf("Inserisci un'opzione valida!\n");
+        } while(scelta != 1 && scelta != 2);
 
-    if(scelta == 1) {
-        //da file
-    } else {
-        //da terminale
+        if(scelta == 1) {
+            //da file
+            char path[MAX];
+            FILE *file;
 
-    }
+            do {
+                printf(WARNING "\nIl file deve riportare i valori separati da uno spazio e avere un'estensione .txt" RESET);
 
+                printf("\nInserisci il path del file .txt: ");
+                fflush(stdin);
+                fgets(path, sizeof(path), stdin);
 
-    printf("\n\n");
+                //remove \n
+                if (path[strlen(path) - 1] == '\n') {
+                    path[strlen(path) - 1] = '\0';
+                }
+
+                if((file = fopen(path, "r")) != NULL) {
+                    printf("Utilizzo il file: " PATH "%s\n" RESET, path);
+                } else {
+                    printf("Impossibile trovare il file: " PATH "%s\n" RESET, path);
+                }
+            } while(file == NULL);
+
+            fclose(file);
+        } else {
+            //da terminale
+        }
+
+        /*if(aspettiamo())
+            printf("\nAspettiamo");
+        else
+            printf("\nCambiamo ristorante");
+        */
+        do {
+            printf("\nVuoi riprovare (Y/n)? ");
+            scanf(" %1c", &retry);
+            if(retry != 'y' && retry != 'Y' && retry != 'N' && retry != 'n') //alert
+                printf("Inserisci Y (si) o n (no)\n");
+        } while(retry != 'y' && retry != 'Y' && retry != 'N' && retry != 'n');
+    } while(retry != 'n' && retry != 'N');
+
+    printf("\nTerminato con successo ...");
     return 0;
+}
+
+bool aspettiamo(Ristorante) {
+    bool response;
+
+    return response;
 }
